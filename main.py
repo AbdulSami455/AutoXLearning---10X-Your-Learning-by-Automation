1dca53fe99f8f0af6e0b1f7f4c3e8a1351209a99
sami <msami.bese22seecs@seecs.edu.pk>
2023-06-25 02:14:53 +0500

    Main Window Coding

--- main.py
+++ main.py
@@ -1,7 +1,43 @@
-# This is a sample Python script.
+import tkinter as tk
+import customtkinter
+from PIL import ImageTk, Image
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Main Window GUI
+loginpage = tk.Tk()
+loginpage.geometry("1000x500")
+loginpage.title("AutoX Login Now")
+
+# Photo in Background
+image = Image.open("6263767_3236267-removebg-preview.png")
+image = image.resize((700, 400))
+
+photo = ImageTk.PhotoImage(image)
+label = tk.Label(loginpage, image=photo)
+label.place(x=-60, y=50)
+
+
+#Username Label and Entry
+label1 = tk.Label(loginpage, text="Username:")
+label1.place(x=550, y=200)
+
+entry1 = tk.Entry(loginpage, width=40)
+entry1.place(x=650, y=200)
+
+label2 = tk.Label(loginpage, text="Password:")
+label2.place(x=550, y=260)
+
+#Password Label and Entry
+entry2 = tk.Entry(loginpage, width=40)
+entry2.place(x=650, y=260)
+
+#Header Label
+header_label = tk.Label(loginpage, text="Login With Firebase", font=("Arial", 24, "bold"))
+header_label.place(x=650, y=100)
+
+
+loginpage.mainloop()
+
+
 
 
 
