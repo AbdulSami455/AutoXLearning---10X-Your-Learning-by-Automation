b324c4b540665efe4efc69db328292124357abca
sami <msami.bese22seecs@seecs.edu.pk>
2023-06-28 07:51:00 +0500

    Adding Button

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 import customtkinter
 from PIL import ImageTk, Image
-
+import authentication as auth
 # Main Window GUI
 loginpage = tk.Tk()
 loginpage.geometry("1000x500")
@@ -29,11 +29,14 @@ label2.place(x=550, y=260)
 #Password Label and Entry
 entry2 = tk.Entry(loginpage, width=40)
 entry2.place(x=650, y=260)
-
 #Header Label
 header_label = tk.Label(loginpage, text="Login With Firebase", font=("Arial", 24, "bold"))
 header_label.place(x=650, y=100)
 
+
+
+login_button = tk.Button(loginpage, text="Login",command=login)
+login_button.place(x=650, y=320)
 
 loginpage.mainloop()
 
