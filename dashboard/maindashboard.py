d2bbb8230ef510022e02f847e734e6268fb7e3c3
sami <msami.bese22seecs@seecs.edu.pk>
2023-07-20 03:26:03 +0500

    making dashboard

--- dashboard/maindashboard.py
+++ dashboard/maindashboard.py
@@ -2,5 +2,5 @@ import customtkinter as ctk
 def dashboardf():
      dash=ctk.CTk()
      dash.geometry("1000x600")
-     search=ctk.CTkEntry(dash)
-     search.place(x=20,y=20)
+     search=ctk.CTkEntry(dash,width=500,height=10)
+     search.place(x=80,y=20)
