6b6421067fb2e1bd59187411537acb262d2d9db6
sami <msami.bese22seecs@student.nust.edu.pk>
2023-07-30 13:29:22 +0500

    making dashboard

---
+++ dashboard/codewebsites.py
@@ -0,0 +1,13 @@
+import webbrowser
+
+def codeacademy():
+    webbrowser.open("https://www.codecademy.com/")
+
+def freecodecamp():
+    webbrowser.open("https://www.freecodecamp.org/")
+
+def coursera():
+    webbrowser.open("https://www.coursera.org/")
+
+def w3schools():
+    webbrowser.open("")
