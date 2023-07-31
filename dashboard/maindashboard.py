fefc6ed8239307c18e8e6673064ea8117b0c3b7f
sami <msami.bese22seecs@student.nust.edu.pk>
2023-07-31 16:04:35 +0500

    making dashboard

--- dashboard/maindashboard.py
+++ dashboard/maindashboard.py
@@ -63,9 +63,12 @@ def dashboardf():
      pythonbutton.place(x=90, y=220)
      pythonbutton.configure(width=45, height=40, highlightthickness=0, highlightbackground='SystemButtonFace')
 
-
-     javabutton=ctk.CTkButton(dash,text="Hello",command=searchnow)
-     javabutton.place(x=150,y=220)
+     image2 = Image.open("Java-Logo.png")
+     image2 = image2.resize((45, 40), Image.ANTIALIAS)
+     image_tk2 = ImageTk.PhotoImage(image2)
+     javabutton = ctk.CTkButton(dash, image=image_tk2, text="Hello", compound=ctk.TOP)
+     javabutton.place(x=150, y=220)
+     javabutton.configure(width=45, height=40, highlightthickness=0, highlightbackground='SystemButtonFace')
 
 
 
