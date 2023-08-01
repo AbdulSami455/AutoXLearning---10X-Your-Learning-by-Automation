0d2247b90232efcb164033529a83fbb1ceebfdbb
sami <msami.bese22seecs@student.nust.edu.pk>
2023-08-02 00:17:42 +0500

    making dashboard

--- dashboard/downloadfiles.py
+++ dashboard/downloadfiles.py
@@ -7,3 +7,15 @@ import webbrowser
 
 def pythondownload():
     webbrowser.open("https://media.readthedocs.org/pdf/python-guide/latest/python-guide.pdf")
+
+def javadownload():
+    webbrowser.open("https://docs.marklogic.com/guide/java.pdf")
+
+def javascriptdownload():
+    webbrowser.open("https://opensource.adobe.com/dc-acrobat-sdk-docs/acrobatsdk/pdfs/acrobatsdk_jsapiref.pdf")
+
+def typescriptdownload():
+    webbrowser.open("https://www.staging-typescript.org/assets/typescript-handbook.pdf")
+
+def godonwload():
+    webbrowser.open("https://www.golang-book.com/public/pdf/gobook.pdf")
